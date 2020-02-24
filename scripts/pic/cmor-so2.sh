15bb13d5b2dee2cc11f12e76370f9f51b3cf6b34
mnichol3 <nicholson.matt94@gmail.com>
2020-02-24 16:00:33 -0500

    Add user-defined cmip6 metadata file path

--- scripts/pic/cmor-so2.sh
+++ scripts/pic/cmor-so2.sh
@@ -16,14 +16,16 @@ module load gcc          # NetCDF requires a compiler module to be loaded as wel
 module load netcdf       # The cmor-ize script requires netCDF3 or netCDF4
 
 # Initialize directory variables -----------------------------------------------
-VAR="SO2"                                   # Variable(s) to convert
-DIR_ROOT="/pic/projects/GCAM/mnichol/emip"  # Root EMIP project directory on pic
-DIR_TEST="lib/cmor/cmor/Test"               # Cmor repo Test directory
-DIR_TABLES="lib/cmor/cmor/Tables"           # Cmor repo Tables directory
-DIR_PREFIX="model-output/pnnl-cesm"         # $DIR_ROOT/model-output/pnnl-cesm
-DIR_INPUT="run"                             # $DIR_ROOT/model-output/pnnl-cesm/<model>/run
-DIR_OUTPUT="cmip6"                          # $DIR_ROOT/model-output/pnnl-cesm/<model>/cmip6
-DIR_HANDLER="cmor_handlers"                 # $DIR_ROOT/cmor_handlers
+VAR="SO2"                                     # Variable(s) to convert
+DIR_ROOT="/pic/projects/GCAM/mnichol/emip"    # Root EMIP project directory on pic
+DIR_TEST="lib/cmor/cmor/Test"                 # Cmor repo Test directory
+DIR_TABLES="lib/cmor/cmor/Tables"             # Cmor repo Tables directory
+DIR_PREFIX="model-output/pnnl-cesm"           # $DIR_ROOT/model-output/pnnl-cesm
+DIR_INPUT="run"                               # $DIR_ROOT/model-output/pnnl-cesm/<model>/run
+DIR_OUTPUT="cmip6"                            # $DIR_ROOT/model-output/pnnl-cesm/<model>/cmip6
+DIR_HANDLER="lib/e3sm_to_cmip/cmor_handlers"  # $DIR_ROOT/lib/e3sm_to_cmip/cmor_handlers
+DIR_USR_IN="lib/e3sm_to_cmip/user_input/e3sm_user_config_emip.json"  # User-defined CMIP6 metadata file
+# List of models whose output we need to convert
 MODELS=("FAMIPC5" "FAMIPC5_MERRA2" "FAMIPC5_MERRA2_SO4_NSEAS" "FAMIPC5_SO4_NSEAS")
 
 # Formulate the function call & pray -------------------------------------------
@@ -33,5 +35,5 @@ for MODEL in "${MODELS[@]}"; do
    echo "Processing $MODEL..."
    CURR_IN="$DIR_PREFIX/$MODEL/$DIR_INPUT"
    CURR_OUT="$DIR_PREFIX/$DIR_OUTPUT"
-   e3sm_to_cmip -v $VAR -i $CURR_IN -o $CURR_OUT --tables $DIR_TABLES --handlers $DIR_HANDLER --user-input ????
+   e3sm_to_cmip -v $VAR -i $CURR_IN -o $CURR_OUT --tables $DIR_TABLES --handlers $DIR_HANDLER --user-input $DIR_USR_IN
 done
