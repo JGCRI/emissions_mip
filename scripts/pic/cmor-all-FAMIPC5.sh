3a67980217818bb36cf68a587135eb4699913aa0
mnichol3 <nicholson.matt94@gmail.com>
2020-02-26 15:46:56 -0500

    I dont know what I'm doing

--- scripts/pic/cmor-all-FAMIPC5.sh
+++ scripts/pic/cmor-all-FAMIPC5.sh
@@ -26,19 +26,14 @@ module load netcdf       # The cmor-ize script requires netCDF3 or netCDF4
 DIR_ROOT="/pic/projects/GCAM/mnichol/emip"    # Root EMIP project directory on pic
 DIR_TEST="lib/cmor/cmor/Test"                 # Cmor repo Test directory
 DIR_TABLES="lib/cmor/cmor/Tables"             # Cmor repo Tables directory
-DIR_PREFIX="model-output/pnnl-cesm"           # $DIR_ROOT/model-output/pnnl-cesm
-DIR_INPUT="run"                               # $DIR_ROOT/model-output/pnnl-cesm/<model>/run
-DIR_OUTPUT="cmip6"                            # $DIR_ROOT/model-output/pnnl-cesm/<model>/cmip6
 DIR_HANDLER="lib/e3sm_to_cmip/cmor_handlers"  # $DIR_ROOT/lib/e3sm_to_cmip/cmor_handlers
 DIR_USR_IN="lib/e3sm_to_cmip/user_input/e3sm_user_config_emip.json"  # User-defined CMIP6 metadata file
-# List of models whose output we need to convert
-MODELS=("FAMIPC5" "FAMIPC5_MERRA2" "FAMIPC5_MERRA2_SO4_NSEAS" "FAMIPC5_SO4_NSEAS")
 
 # Formulate the function call & pray -------------------------------------------
 cd $DIR_ROOT
 
 echo "Processing FAMIPC5..."
 
-CURR_IN="$DIR_PREFIX/FAMIPC5/$DIR_INPUT"
-CURR_OUT="$DIR_PREFIX/FAMIPC5/$DIR_OUTPUT"
-e3sm_to_cmip -v ['all'] -i $CURR_IN -o $CURR_OUT -t $DIR_TABLES -H $DIR_HANDLER -u $DIR_USR_IN
+DIR_INPUT="model-output/pnnl-cesm/FAMIPC5/run"
+DIR_OUTPUT="model-output/pnnl-cesm/FAMIPC5/cmip6"
+e3sm_to_cmip -v 'all' -i $DIR_INPUT -o $DIR_OUTPUT -t $DIR_TABLES -H $DIR_HANDLER -u $DIR_USR_IN
