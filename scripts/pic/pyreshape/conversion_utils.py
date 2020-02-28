3b3b9684f4c8c274dee01a1eecbf05e3f052fcba
mnichol3 <nicholson.matt94@gmail.com>
2020-02-28 14:31:44 -0500

    Initial commit

---
+++ scripts/pic/pyreshape/conversion_utils.py
@@ -0,0 +1,56 @@
+"""
+Utility functions for converting CESM time-slice output files to time-series
+
+Matt Nicholson
+28 Feb 2020
+"""
+import os
+import re
+
+def parse_output_suffix(input_files):
+    """
+    Given a list of input files, create the output file suffix
+    
+    Ex: 
+        input_files   = ['FAMIPC5.cam.h0.2010-09.nc', 'FAMIPC5.cam.h0.2010-10.nc']
+        output_suffix = '201009-201010.nc'
+    
+    Params
+    -------
+    input_files : list of str
+        List of input time-slice files to convert
+    
+    Return
+    -------
+    str
+    """
+    d_first = parse_file_date(input_files[0])
+    d_first = d_first.repalce('-', '')
+    d_last  = parse_file_date(input_files[-1])
+    d_last  = d_last.repalce('-', '')
+    ret_val = '.{}-{}.nc'.format(d_first, d_last)
+    return ret_val
+
+def parse_file_date(fname):
+    """
+    Extract the date from a CESM history file filename. Fails if unable to extract
+    the date from the filename.
+    
+    Params
+    -------
+    fname : str
+        CESM history filename
+    
+    Return
+    -------
+    str
+        Date corresponding to the CESM history file in the format YYYYMM
+    """
+    pattern = r'\.(\d{4}-\d{2})\.nc'
+    match = re.search(pattern, fname)
+    if (match):
+        ret_val = match.group(1)
+    else:
+        raise ValueError('Unable to extract date from CESM history filename')
+    return ret_val
+        
---
+++ scripts/pic/pyreshape/reshape-famipc5.py
@@ -0,0 +1,51 @@
+"""
+Convert CESM FAMIPC5 time-slice output to time-series output via PyReshape
+
+Matt Nicholson
+28 Feb 2020
+"""
+from pyreshape import specification, reshaper
+
+import conversion_utils
+
+# --- Set up -------------------------------------------------------------------
+class dirs:
+    ROOT   = '/pic/projects/GCAM/mnichol/emip'
+    INPUT  = 'model-output/pnnl-cesm/FAMIPC5/run'
+    OUTPUT = 'model-output/pnnl-cesm/FAMIPC5/cmip6'
+    
+# Change working directory to root project path
+os.chdir(dirs.ROOT)
+
+# Define which model output history files we want to convert
+input_fnames = ['FAMIPC5.cam.h0.2010-09.nc', 'FAMIPC5.cam.h0.2010-10.nc']
+input_files  = [os.path.join(dirs.INPUT, f) for f in input_fnames]
+
+# Converted time-series file prefix & suffix
+prefix = 'FAMIPC5.cam.'
+output_prefix = os.path.join(dirs.OUTPUT, prefix)
+output_suffix = conversion_utils.parse_output_suffix(input_fnames)
+
+# --- Create PyReshaper specifier object ---------------------------------------
+specifier = specification.create_specifier()
+
+# Define specifier input needed perform the conversion
+specifier.input_file_list = input_files
+specifier.netcdf_format = "netcdf4"
+specifier.compression_level = 1
+specifier.output_file_prefix = output_prefix
+specifier.output_file_suffix = output_suffix
+specifier.time_variant_metadata = ["time", "time_bounds"]
+# specifier.exclude_list = ['HKSAT','ZLAKE']
+
+# Create the PyReshaper object
+rshpr = reshaper.create_reshaper(specifier,
+                                 serial=False,
+                                 verbosity=1,
+                                 wmode='s')
+
+# --- Run the conversion (slice-to-series) process -----------------------------
+rshpr.convert()
+
+# --- Print timing diagnostics -------------------------------------------------
+rshpr.print_diagnostics()
