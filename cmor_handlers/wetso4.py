e4f0e67ee776e307c0f5240b98bc64e1cb263513
mnichol3 <nicholson.matt94@gmail.com>
2020-03-03 13:20:38 -0500

    Initial commit

---
+++ cmor_handlers/wetso4.py
@@ -0,0 +1,75 @@
+"""
+e3sm_to_cmip cmor handler script
+
+Handler for Wet Deposition Rate of SO4 (wetso4)
+
+Input Variable(s)
+------------------
+* so4_a1SFWET : Wet deposition flux at surface of so4_a1, in kg m-2 s-1
+* so4_a2SFWET : Wet deposition flux at surface of so4_a2, in kg m-2 s-1
+* so4_a3SFWET : Wet deposition flux at surface of so4_a3, in kg m-2 s-1
+* so4_c1SFWET : Wet deposition flux at surface of so4_c1, in kg m-2 s-1
+* so4_c2SFWET : Wet deposition flux at surface of so4_c2, in kg m-2 s-1
+* so4_c3SFWET : Wet deposition flux at surface of so4_c3, in kg m-2 s-1
+
+Matt Nicholson
+3 Mar 2020
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import cmor
+from e3sm_to_cmip.lib import handle_variables
+
+# list of raw variable names needed
+RAW_VARIABLES = ['so4_a1SFWET', 'so4_a2SFWET', 'so4_a3SFWET',
+                 'so4_c1SFWET', 'so4_c2SFWET', 'so4_c3SFWET']
+VAR_NAME = 'wetso4'
+VAR_UNITS = 'kg m-2 s-1'
+TABLE = str('CMIP6_AERmon.json')
+LEVELS = {
+    'name' : 'lev',
+    'units': 'hPa',
+    'e3sm_axis_name': 'lev'
+}
+
+
+def write_data(varid, data, timeval, timebnds, index, **kwargs):
+    """
+    wetso4 = so4_a1SFWET + so4_c1SFWET + so4_a2SFWET + so4_c2SFWET + so4_a3SFWET + so4_c3SFWET
+    """
+    outdata = data['so4_a1SFWET'][index, :]
+    for var in RAW_VARIABLES[1:]:
+        outdata = outdata + data[var][index, :]
+        
+    cmor.write(
+        varid,
+        outdata,
+        time_vals=timeval,
+        time_bnds=timebnds)
+# ------------------------------------------------------------------
+
+
+def handle(infiles, tables, user_input_path, **kwargs):
+    """
+    Parameters
+    ----------
+        infiles (List): a list of strings of file names for the raw input data
+        tables (str): path to CMOR tables
+        user_input_path (str): path to user input json file
+    Returns
+    -------
+        var name (str): the name of the processed variable after processing is complete
+    """
+    return handle_variables(
+        metadata_path=user_input_path,
+        tables=tables,
+        table=TABLE,
+        infiles=infiles,
+        raw_variables=RAW_VARIABLES,
+        write_data=write_data,
+        outvar_name=VAR_NAME,
+        outvar_units=VAR_UNITS,
+        serial=kwargs.get('serial'),
+        levels=LEVELS,
+        logdir=kwargs.get('logdir'))
+# ------------------------------------------------------------------
