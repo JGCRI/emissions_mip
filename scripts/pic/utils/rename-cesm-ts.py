bdf5526f4fd494d9d83ec516bdda0fb7da498946
mnichol3 <matthew.nicholson@pnnl.gov>
2020-03-25 12:24:50 -0400

    Fix new filename format

--- scripts/pic/utils/rename-cesm-ts.py
+++ scripts/pic/utils/rename-cesm-ts.py
@@ -1,6 +1,8 @@
 """
 Rename CESM timeseries files to conform to the format that the 
-e3sm_to_cmip package expects for input files
+e3sm_to_cmip package expects for input files.
+
+e3sm_to_cmip input file format: {e3sm_var}_YYYYMM_YYYYMM.nc
 
 Usage
 ------
@@ -79,14 +81,13 @@ def rename_files(dir, files)
         List of paths of renamed files
     """
     def _rename_file(dir, fname):
-        f_old = os.path.join(dir, fname)
+        fname_old = os.path.join(dir, fname)
         splits = fname.split('.')
         splits[3] = splits[3].replace('-', '_')
-        fname_new = '_'.join(splits[:-1])
-        fname_new = fname_new + '.nc'
+        fname_new = '{}_{}.nc'.format(splits[2], splits[3].replace('-', '_'))
         print('{} --> {}'.format(fname, fname_new))
         f_new = os.path.join(dir, fname_new)
-        os.rename(f_old, f_new)
+        os.rename(fname_old, fname_new)
         return f_new
     new_fnames = [_rename_file(dir, f) for f in files]
     return new_fnames
