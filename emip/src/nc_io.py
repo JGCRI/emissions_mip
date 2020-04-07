42a22b5b05c680a5cd5e1adc673bb019415b4c49
mnichol3 <matthew.nicholson@pnnl.gov>
2020-04-07 16:39:49 -0400

    WIP

--- emip/src/nc_io.py
+++ emip/src/nc_io.py
@@ -15,18 +15,18 @@ def ncdump(nc_fid, verb=True):
     Parameters
     ----------
     nc_fid : netCDF4.Dataset
-        A netCDF4 dateset object
+        A netCDF4 dateset object.
     verb : Boolean
-        whether or not nc_attrs, nc_dims, and nc_vars are printed
+        whether or not nc_attrs, nc_dims, and nc_vars are printed.
 
     Returns
     -------
     nc_attrs : list
-        A Python list of the NetCDF file global attributes
+        A Python list of the NetCDF file global attributes.
     nc_dims : list
-        A Python list of the NetCDF file dimensions
+        A Python list of the NetCDF file dimensions.
     nc_vars : list
-        A Python list of the NetCDF file variables
+        A Python list of the NetCDF file variables.
         
     Credit: Chris Slocum, 22 Jul 2014
     Adapted to Python 3 by Matt Nicholson
@@ -77,13 +77,63 @@ def ncdump(nc_fid, verb=True):
 
 def read_nc(nc_path):
     """
-    Read a netCDF file
+    Read a netCDF file into a Dataset object.
     
     Parameters
     -----------
     nc_path : str
-        Path of the netCDF file to read
+        Path of the netCDF file to read.
+        
+    Returns
+    -------
+    NetCDF4 Dataset object
     """
     nc = netCDF4.Dataset(nc_path, 'r')
     return nc
+    
+    
+def get_var(nc_dataset, var_name):
+    """
+    Retrieve a variable from a netCDF Dataset object
+    
+    Parameters
+    -----------
+    nc_dataset : NetCDF4 Dataset object
+        NetCDF Dataset to retrieve the variable from.
+    var_name : str
+        Variable to retrieve from the netCDF Dataset.
+        
+    Returns
+    -------
+    NetCDF4 Variable object
+    """
+    var = nc_dataset.variables[var_name]
+    return var
+    
+    
+def get_var_from_file(nc_fname, var_name):
+    """
+    Read and retrieve a variable from a netCDF file
+    
+    Parameters
+    ----------
+    nc_fname : str
+        NetCDF file to open and read.
+    var_name : str
+        Variable to retrieve from the netCDF Dataset.
+        
+    Returns
+    -------
+    NetCDF4 Variable object
+    
+    Raises
+    ------
+    FileNotFoundError
+        If the netCDF file specified by "nc_fname" is not found.
+    """
+    nc = Dataset(nc_fname, 'r')
+    var = nc_dataset.variables[var_name]
+    nc.close()
+    return var
+    
     
--- emip/src/nc_var.py
+++ emip/src/nc_var.py
@@ -1,63 +1,73 @@
 """
-Class to represent a single variable from a netCDF file
+Wrappers for NetCDF4 Variable class functions.
 
 Matt Nicholson
-21 Feb 2020
+7 April 2020
 """
-import numpy as np
 
-class NCVar:
+
+def get_attributes(nc_var):
+    """
+    Retrieve the attributes for a netCDF variable. 
+    Wrapper for netCDF4.Variable.ncattrs().
     
-    def __init__(self, nc_file, var_name, parent_nc):
-        """
-        Constructor for an NCVar instance
+    Parameters
+    ----------
+    nc_var : NetCDF4 Variable object
+        Variable to retrieve attributes from.
+    
+    Returns
+    -------
+    List of str
+        The variable's attributes.
+    """
+    attrs = nc_var.ncattrs()
+    return attrs
+    
+
+def get_attr(nc_var, attr):
+    """
+    Retrieve a netCDF variable attribute.
+    Use if you need to set a netCDF attribute with the same name as one of
+    the reserved python attributes.
+    Wrapper for netCDF4.Variable.getncattr().
+    
+    Parameters
+    ----------
+    nc_var : NetCDF4 Variable object
+        Variable to retrieve attributes from.
+    attr : str
+        Name of the attribute to retrieve.
         
-        Parameters
-        -----------
-        nc_file : netCDF4.Dataset
-            A netCDF4 dateset object
-        var_name : str
-            Name of the variable represented by the instance
-        parent_nc : str 
-            Name of the parent netCDF file from which the variable is being retrieved
-            
-        Instance Attributes
-        --------------------
-        name : str
-            Name of the variable. Ex: 'so4_c2'
-        shape : tuple of int
-            Shape of the variable
-        units : str
-            Variable units
-        long_name : str
-            Variable long name. Ex: 'so4_c2 in cloud water'
-        data : NumPy nd array
-            Variable data
-        """
-        self.parent       = parent_nc
-        self.name         = var_name
-        self.shape        = nc_file.variables[var_name].shape
-        self.units        = nc_file.variables[var_name].units
-        self.long_name    = nc_file.variables[var_name].long_name
-        self.data         = np.asarray(nc_file.variables[var_name][:])
-        
-    def get_name(self):
-        return self.name
+    Returns
+    -------
+    str : NetCDF attribute
     
-    def get_shape(self):
-        return self.shape
-        
-    def get_units(self):
-        return self.units
+    Raises
+    ------
+    AttributeError
+        If the attribute is not found within the variable.
+    """
+    try:
+        return nc_var.getncattr(attr)
+    except AttributeError:
+        raise AttributeError('Attribute not found: {}'.format(attr))
     
-    def get_name_long(self):
-        return self.long_name
     
-    def get_data(self):
-        return self.data
+def get_group(nc_var):
+    """
+    Return the group that the variable is a member of.
+    Wrapper for netCDF4.Variable.group()
     
-    def get_parent(self):
-        return parent
-        
-    def __repr__(self):
-        return "<NCVar object - {}-{}>".format(self.parent, self.name)
+    Parameters
+    ----------
+    nc_var : NetCDF4 Variable object
+        Variable to retrieve attributes from.
+    
+    Returns
+    -------
+    List of str
+        The variable's attributes.
+    """
+    attrs = nc_var.ncattrs()
+    return attrs
