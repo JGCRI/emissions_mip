f6c3624cf7b8574f5ebb6e8e3455b74f375c2a39
mnichol3 <matthew.nicholson@pnnl.gov>
2020-04-08 14:55:37 -0400

    Initial commit

---
+++ emip/src/config.py
@@ -0,0 +1,14 @@
+"""
+Configuration file.
+
+Matt Nicholson
+8 April 2020
+"""
+import os
+
+class DIRS:
+	proj_root = '/pic/projects/GCAM/mnichol/emip'
+    model_output = os.path.join(proj_root, 'model-output')
+    prefix_colum = ('columbia/gpfsm/dnb53/projects/p117/pub/CMIP6/AerChemMIP/NASA-GISS'
+                    'GISS-E2-1-G/piClim-SO2/r1i1p5f103/AERmon')
+    suffix_colum = 'gn/v20191120'
---
+++ emip/src/path_funcs.py
@@ -0,0 +1,73 @@
+"""
+Functions to parse paths and such.
+
+Matt Nicholson
+8 April 2020
+"""
+import os
+from pathlib import Path
+
+import config
+
+
+def get_root():
+    """
+    Get the path of the project's root directory.
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    str
+        Path of the project's root directory.
+    """
+    root = str(Path(os.path.abspath(__file__)).parents[2])
+    return root
+    
+    
+def get_cwd():
+    """
+    Get the current working directory. Wrapper for os.getcwd().
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    str
+        Absolute path of the current working directory.
+    """
+    return os.getcwd()
+    
+    
+def get_var_path(var_name, inst):
+    """
+    Contruct the CMORized path of an output variable netCDF file.
+    
+    Parameters
+    ----------
+    var_name : str
+        Name of the output variable corresponding to the file.
+    inst : str
+        Institution that produced the model output. Corresponds to the directory
+        to start searching in. Ex: 'nasa', 'pnnl', 'colum' or 'columbia'.
+        
+    Return
+    ------
+    str
+        Absolute path of the variable output file.
+    """
+    if inst == 'columbia':
+        inst = 'colum'
+    prefix = getattr(config.DIRS, 'prefix_{}'.format(inst))
+    suffix = getattr(config.DIRS, 'suffix_{}'.format(inst))
+    path = os.path.join(config.DIRS.proj_root, config.DIRS.model_output, prefix,
+                        var_name, suffix)
+    file = [f for f in os.listdir(path) if f.endswith('.nc')]
+    if len(file) > 1:
+        raise OSError('Multiple files found in {}'.format(path))
+    path = os.path.join(path, file[0])
+    return path
