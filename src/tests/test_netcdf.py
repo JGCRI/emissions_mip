77e4b6e499c6ec61efef130e45adf4ee8c5a6ddc
mnichol3 <matthew.nicholson@pnnl.gov>
2020-04-09 17:34:28 -0400

    Rename test file to reflex netcdf class file name change

--- src/tests/test_netcdf.py
+++ src/tests/test_netcdf.py
@@ -13,7 +13,7 @@ import numpy as np
 # Insert src directory to Python path for importing
 sys.path.insert(1, '..')
 
-import nc_file
+import netcdf
 
 
 class TestNetcdf(unittest.TestCase):
@@ -27,13 +27,13 @@ class TestNetcdf(unittest.TestCase):
     def test_basic_1(self):
         """File can be read into object.
         """
-        nc = nc_file.Netcdf(self.nc_fname)
-        self.assertTrue(isinstance(nc, nc_file.Netcdf))
+        nc = netcdf.Netcdf(self.nc_fname)
+        self.assertTrue(isinstance(nc, netcdf.Netcdf))
         
     def test_basic_2(self):
         """Object has correct attributes & values.
         """
-        nc = nc_file.Netcdf(self.nc_fname)
+        nc = netcdf.Netcdf(self.nc_fname)
         test_vars = list(nc.variables.keys())
         self.assertEqual(nc.filename, self.nc_fname)
         self.assertTrue(np.array_equal(self.nc_vars, test_vars))
@@ -47,14 +47,14 @@ class TestNetcdf(unittest.TestCase):
     def test_basic_3(self):
         """Object's variables are present & valid.
         """
-        nc = nc_file.Netcdf(self.nc_fname)
+        nc = netcdf.Netcdf(self.nc_fname)
         var_names = list(nc.variables.keys())
         self.assertEqual(var_names, self.nc_vars)
     
     def test_var_time(self):
         """Time variable is present and valid.
         """
-        nc = nc_file.Netcdf(self.nc_fname)
+        nc = netcdf.Netcdf(self.nc_fname)
         var_actual = self.nc_actual.variables['time']
         var_test   = nc.variables['time']
         self.assertEqual(var_actual.shape, var_test.shape)
@@ -66,7 +66,7 @@ class TestNetcdf(unittest.TestCase):
     def test_var_lat_lon(self):
         """Lat & lon variables are present and valid.
         """
-        nc = nc_file.Netcdf(self.nc_fname)
+        nc = netcdf.Netcdf(self.nc_fname)
         for var in ['lat', 'lon']:
             var_actual = self.nc_actual.variables[var]
             var_test   = nc.variables[var]
@@ -80,13 +80,14 @@ class TestNetcdf(unittest.TestCase):
     def test_var_dryso4(self):
         """DrySO4 variable (the variable we actually care about) is present and valid.
         """
-        nc = nc_file.Netcdf(self.nc_fname)
+        nc = netcdf.Netcdf(self.nc_fname)
         var_actual = self.nc_actual.variables['dryso4']
         var_test   = nc.variables['dryso4']
         self.assertEqual(var_actual.shape, var_test.shape)
         self.assertEqual(var_actual.units, var_test.units)
         self.assertEqual(var_actual.long_name, var_test.long_name)
         self.assertEqual(var_actual.standard_name, var_test.standard_name)
+        
         
 if __name__ == '__main__':
     unittest.main()
