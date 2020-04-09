0414a7cce274a049b35a0a00bd135503c4581c5a
mnichol3 <matthew.nicholson@pnnl.gov>
2020-04-09 17:34:00 -0400

    Rename Netcdf class file

--- src/netcdf.py
+++ src/netcdf.py
@@ -8,7 +8,6 @@ import netCDF4
 import os
 import numpy as np
 
-import nc_io
 import nc_variable
 
 class Netcdf:
@@ -20,28 +19,28 @@ class Netcdf:
         Parameters
         -----------
         nc_fname : str
-            Name of the netCDF file to read
-        nc_vars : str, list of str, optional.
+            Name of the netCDF file to read.
+        nc_vars : str, list of str, optional
             Subset of variables to read from the netCDF file. If not given,
             all variables will be read. Default is to read all variables.
             
         Attributes
         -----------
         filename : str
-            Name of the netCDF file
+            Name of the netCDF file.
         nc_path : str
-            Path of the netCDF file
-        nc_vars : dict of {str : NCVar object}
+            Path of the netCDF file.
+        variables : dict of {str : NetcdfVariable object}
             Dictionary where a key is the string name of a netCDF variable and 
-            the value is the NCVar object corresponding to that variable
+            the value is the NetcdfVariable object corresponding to that variable.
         conventions : str
-            NetCDF conventions of the file being read
+            NetCDF conventions of the file being read.
         case : str
-            Experiment case pertaining to the netCDF data
+            Experiment case pertaining to the netCDF data.
         """
-        self.filename      = self._parse_fname(nc_fname)
-        self.variables     = {}
-        nc = self._read_nc(nc_fname, nc_vars)
+        self.filename  = self._parse_fname(nc_fname)
+        self.variables = {}
+        nc = self._read_nc(nc_fname)
         self._parse_vars(nc, nc_vars)
         nc.close()
         
@@ -59,6 +58,31 @@ class Netcdf:
         """
         attrs = list(self.__dict__.keys())
         return attrs
+        
+    def get_var(self, var_name):
+        """
+        Get a variable from the Netcdf object and return it as a NetcdfVariable object.
+        
+        Parameters
+        ----------
+        var_name : str
+            Name of the variable to get.
+        
+        Returns
+        -------
+        NetcdfVariable object
+        
+        Raises
+        ------
+        KeyError
+            If "var_name" is not present in the Netcdf object's variables dictionary
+            keys.
+        """
+        try:
+            var = self.variables[var_name]
+        except:
+            raise KeyError('Variable "{}" not found in {}'.format(var_name, self.filename))
+        return var
         
     def _parse_fname(self, nc_path):
         """
@@ -96,7 +120,7 @@ class Netcdf:
             new_var = nc_variable.NetcdfVariable(var_obj)
             self.variables[var] = new_var
     
-    def _read_nc(self, nc_file, nc_var=None):
+    def _read_nc(self, nc_file):
         """
         Read a netCDF file.
         
@@ -104,9 +128,6 @@ class Netcdf:
         -----------
         nc_file : str
             Name of the netCDF file to read.
-        nc_vars : str, list of str, optional.
-            Names of variables to extract from the netCDF file. If not given,
-            all variables will be read. Default is to read all variables.
         
         Return
         ------
