5df4f02dc509f1fb1b41ef41320785801b16e526
mnichol3 <matthew.nicholson@pnnl.gov>
2020-04-16 15:38:15 -0400

    Re-format for easier editing

--- scripts/pic/append_coord_bounds.sh
+++ scripts/pic/append_coord_bounds.sh
@@ -12,12 +12,31 @@
 #SBATCH --mail-user matthew.nicholson@pnnl.gov
 #SBATCH --mail-type END
 
-source activate emip
+source activate pyreshaper
 
 DIR_ROOT="/pic/projects/GCAM/mnichol/emip/model-output/pnnl-cesm"
-MODELS=("FAMIPC5" "FAMIPC5_MERRA2" "FAMIPC5_MERRA2_SO4_NSEAS" "FAMIPC5_SO4_NSEAS")
 
-for MODEL in "${MODELS[@]}"; do
-   echo "Processing $MODEL..."
-   IN_DIR="$DIR_ROOT/$MODEL/timeseries"
-   python append_coord_bounds.py $IN_DIR
+# --- FAMIPC5 output -----------------------------------------------------------
+MODEL="FAMIPC5"
+echo "Processing $MODEL..."
+IN_DIR="$DIR_ROOT/$MODEL/timeseries"
+python append_coord_bounds.py $IN_DIR
+
+# --- FAMIPC5_MERRA2 output ----------------------------------------------------
+MODEL="FAMIPC5_MERRA2"
+echo "Processing $MODEL..."
+IN_DIR="$DIR_ROOT/$MODEL/timeseries"
+python append_coord_bounds.py $IN_DIR
+
+# --- FAMIPC5_MERRA2_SO4_NSEAS output ------------------------------------------
+MODEL="FAMIPC5_MERRA2_SO4_NSEAS"
+echo "Processing $MODEL..."
+IN_DIR="$DIR_ROOT/$MODEL/timeseries"
+python append_coord_bounds.py $IN_DIR
+
+# --- FAMIPC5_SO4_NSEAS output -------------------------------------------------
+MODEL="FAMIPC5_SO4_NSEAS"
+echo "Processing $MODEL..."
+IN_DIR="$DIR_ROOT/$MODEL/timeseries"
+python append_coord_bounds.py $IN_DIR
+
